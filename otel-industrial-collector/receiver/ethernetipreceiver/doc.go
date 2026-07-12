dfc34f089514fc2b15692782d23680423e9c6dd0
Lukasz Ciukaj <lukasz.ciukaj@gmail.com>
2026-07-12 19:30:51 -0400

    Add metadata.yaml, generated metrics code, and Makefile for ethernetipreceiver

---
+++ otel-industrial-collector/receiver/ethernetipreceiver/doc.go
@@ -0,0 +1,5 @@
+// Package ethernetipreceiver implements a receiver that can be used to collect
+// telemetry from EtherNet/IP (CIP) enabled industrial devices.
+//
+//go:generate mdatagen metadata.yaml
+package ethernetipreceiver
---
+++ otel-industrial-collector/receiver/ethernetipreceiver/go.mod
@@ -0,0 +1,3 @@
+module github.com/otel-industrial/otel-industrial-resources/otel-industrial-collector/receiver/ethernetipreceiver
+
+go 1.26.3
